57fd0b3c3894c07eab20a15778fd10b8e5677622
Kay <okay9ks@gmail.com>
2025-07-03 11:18:19 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,7 @@
+url = '' #Insert URL of concert site (e.g. https://www.cityline.com.hk/Login.html?targetUrl=https%3A%2F%2Fcultural.cityline.com.hk%2Fen%2F2025%2Fphantom2025.html)
+email = '' #Replace with your member email address
+password '' #Replace with your member password
+
+lgbtn = document.getElementByClassName("login-btn theme-btn submit-btn ant-btn");
+
 
